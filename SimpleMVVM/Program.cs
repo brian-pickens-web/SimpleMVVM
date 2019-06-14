38ffaae78d4bde48b59d7da0217e3520dc0f8e02
Pickens, Brian M <brian.m.pickens@gmail.com>
2019-06-14 11:42:27 -0500

    Add simple program initialization class

---
+++ SimpleMVVM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using SimpleMVVM.Framework;
+
+namespace SimpleMVVM
+{
+    // ReSharper disable once IdentifierTypo
+    // ReSharper disable once InconsistentNaming
+    public static class Program 
+    {
+
+        public static void SetView(IViewModelNavigator navigator, Window window)
+        {
+            navigator.NavigateToView(window);
+        }
+
+        public static void Run(IServiceProvider svcProvider, Window window)
+        {
+            // Set Initial View
+            var viewNavigator = svcProvider.GetService(typeof(IViewModelNavigator)) as IViewModelNavigator;
+            viewNavigator.NavigateToView(window);
+
+            // Start App
+            var app = svcProvider.GetService(typeof(Application)) as Application;
+            app.Run(window);
+        }
+
+    }
+}
